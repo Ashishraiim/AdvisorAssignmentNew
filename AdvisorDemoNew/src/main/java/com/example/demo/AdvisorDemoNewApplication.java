a9db6a6dbe81e9e8e858d22b073da1e2837b3114
Ashish <Ashish@LAPTOP-T3N72U55>
2024-01-29 15:01:03 +0530

    Commit for security implementation

--- AdvisorDemoNew/src/main/java/com/example/demo/AdvisorDemoNewApplication.java
+++ AdvisorDemoNew/src/main/java/com/example/demo/AdvisorDemoNewApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @SpringBootApplication
-public class AdvisorDemoNewApplication {
+public class AdvisorDemoNewApplication implements WebSecurityConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvisorDemoNewApplication.class, args);
+	}
+
+	@Override
+	public void init(SecurityBuilder builder) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configure(SecurityBuilder builder) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
